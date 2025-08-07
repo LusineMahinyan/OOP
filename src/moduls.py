6cd8831c0c8fd0cb072c56cf0eeefec4bc0c4bf0
Лусине <lumahinyan@mail.ru>
2025-08-08 01:01:02 +0300

    Creating classes

---
+++ src/moduls.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name: str, description: str, price: float, quantity: int):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    name: str
+    description: str
+    products: List[Product]
+
+    category_count: int = 0
+    product_count: int = 0
+
+    def __init__(self, name: str, description: str, products: List[Product]):
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.category_count += 1
+        Category.product_count += len(products)
